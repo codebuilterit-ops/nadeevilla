f53d22af4ecd09d48f4158c0d2abd9e6b303e7c1
codebuilterit-ops <codebuilterit@gmail.com>
2025-11-23 20:09:22 +0530

    Add files via upload

---
+++ src/assets/Assest.js
@@ -0,0 +1,11 @@
+import logo from './logo.jpg';
+
+import contact from './contact.jpg';
+
+
+export const assets={
+    logo,
+    
+    contact
+}
+
---
+++ src/assets/Gallery/galler.js
@@ -0,0 +1,26 @@
+
+import galler1 from './galler1.jpeg';
+import galler3 from './galler3.jpeg';
+import galler4 from './galler4.jpeg';
+import galler5 from './galler5.jpeg';
+import galler6 from './galler6.jpeg';
+import galler7 from './galler7.jpg';
+import galler2 from './galler2.jpeg';
+import galler8 from './galler8.jpg'
+import galler9 from './galler9.jpg'
+import image from './image.png'
+const galler= [
+  
+  galler1,
+  galler3,
+  galler4,
+  galler5,
+  galler6,
+  galler7,
+  galler2,
+  galler8,
+  galler9,
+  image
+];
+
+export default galler;
