12f020f5021cca2c9ed2c5a87864cb81397ac1ea
codebuilterit-ops <codebuilterit@gmail.com>
2025-11-23 20:30:25 +0530

    Add files via upload

---
+++ src/assets/HomepageImages/home.js
@@ -0,0 +1,45 @@
+import home1 from "./home1.png";
+import home2 from "./home2.png";
+import home3 from "./home3.png";
+import home4 from "./home4.png";
+import home5 from "./home5.png";
+import home6 from './home6.png';
+
+
+
+const homepageImages = [
+    {
+         title: "Nade Villas & Tours",
+    subtitle: "Start your journey with us",
+    background:home1,
+    },
+    {
+        title: "Nade Villas & Tours",
+    subtitle: "Start your journey with us",
+    background:home2,
+    },
+    {
+          title: "Nade Villas & Tours",
+    subtitle: "Start your journey with us",
+    background:home3,
+    },
+    {
+          title: "Nade Villas & Tours",
+    subtitle: "Start your journey with us",
+    background:home4,
+    },
+    {
+          title: "Nade Villas & Tours",
+    subtitle: "Start your journey with us",
+    background:home5,
+    },
+    {
+          title: "Nade Villas & Tours",
+    subtitle: "Start your journey with us",
+    background:home6,
+    },
+  
+
+];
+
+export default homepageImages;
